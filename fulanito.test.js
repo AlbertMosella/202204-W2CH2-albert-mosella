1a0ce2214dd3f51e28c36d4a5d4655759750039a
AlbertMosella <mosella.albert@gmail.com>
2022-04-12 19:48:49 +0200

    All test checked green

--- fulanito.test.js
+++ fulanito.test.js
@@ -22,4 +22,77 @@ describe("Given the stricEquals function", () => {
       expect(expectedResult).toBeFalsy;
     });
   });
+
+  describe("When it receives (0, -0)", () => {
+    test("It should return true", () => {
+      const a = 0;
+      const b = -0;
+
+      const expectedResult = strictEquals(a, b);
+
+      expect(expectedResult).toBeTruthy;
+    });
+  });
+
+  describe("When it receives (-0, 0)", () => {
+    test("It should return true", () => {
+      const a = -0;
+      const b = 0;
+
+      const expectedResult = strictEquals(a, b);
+
+      expect(expectedResult).toBeTruthy;
+    });
+  });
+
+  describe("When it receives (1, 1)", () => {
+    test("It should return true", () => {
+      const a = 1;
+      const b = 1;
+
+      const expectedResult = strictEquals(a, b);
+
+      expect(expectedResult).toBeTruthy;
+    });
+  });
+  describe("When it receives (1, '1')", () => {
+    test("It should return false", () => {
+      const a = 1;
+      const b = "1";
+
+      const expectedResult = strictEquals(a, b);
+
+      expect(expectedResult).toBeFalsy;
+    });
+  });
+  describe("When it receives (true, false)", () => {
+    test("It should return false", () => {
+      const a = true;
+      const b = false;
+
+      const expectedResult = strictEquals(a, b);
+
+      expect(expectedResult).toBeFalsy;
+    });
+  });
+  describe("When it receives (false, false)", () => {
+    test("It should return true", () => {
+      const a = false;
+      const b = false;
+
+      const expectedResult = strictEquals(a, b);
+
+      expect(expectedResult).toBeTruthy;
+    });
+  });
+  describe("When it receives ('Water', 'oil')", () => {
+    test("It should return false", () => {
+      const a = "Water";
+      const b = "Oil";
+
+      const expectedResult = strictEquals(a, b);
+
+      expect(expectedResult).toBeFalsy;
+    });
+  });
 });
